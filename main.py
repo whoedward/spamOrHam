097abe1b49f188bf0a1e8a2fc6a93e46ab63ccb3
ethanCrobison <ethanCrobison@gmail.com>
2015-05-03 22:04:13 -0700

    rearranged directories

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 ## Ethan Robison, Edward Hu
 ## EECS 349 Spring 2015
 
+import sys, os
+from collections import defaultdict
+# from blah import *
+spam_directory = sys.argv[1]
+#clean_directory = sys.argv[2]
+#test_directory = sys.argv[3]
 
+
+# check to see that each directory has no subdirectories
+#root_S = [root for root, _, _ in os.walk(spam_directory)]
+#root_C = [root for root, _, _ in os.walk(spam_directory)]
+#root_T = [root for root, _, _ in os.walk(spam_directory)]
+
+
+for root, dirs, files in os.walk(spam_directory):
+    path = root.split('/')
+    print(len(path) - 1)*'---', os.path.basename(root)
+    for f in files:
+        print(len(path))*'---', f
+    
