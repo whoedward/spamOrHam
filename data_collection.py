cbe50132b91e83b44c00dce5bdba134624ad78c4
ethanCrobison <ethanCrobison@gmail.com>
2015-05-17 17:04:07 -0500

    parse email corpus

--- data_collection.py
+++ data_collection.py
@@ -1,1 +1,21 @@
 ## Ethan Robison, Edward Hu
+import os
+def dir_process(directory):
+    '''walks over a directory and returns the '''
+    emails = []
+    for _, _, files in os.walk('./' + directory):
+        for f in files:
+            with open('./' + directory + '/' + f) as email:
+                emails.append([line.strip() for line in email.readlines()])
+
+    return emails
+
+def split_header(email):
+    '''split an email into header and body'''
+    for w in email:
+        print w.split(" ")
+    return
+
+def email_process(email):
+    '''process the lines in an email according to filter restrictions'''
+    return
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 ## Ethan Robison, Edward Hu
 ## EECS 349 Spring 2015
 
-import sys, os
+import sys##, os
 from collections import defaultdict
-# from data_collection import *
+from data_collection import *
 
-if name == '__main__':
-    print("Hello, world")
+## command line inputs
+spam_dir = sys.argv[1]
+ham_dir = sys.argv[2]
+
+
+if __name__ == '__main__':
+    ham_emails = dir_process(ham_dir)
+    ##    spam_emails = dir_process(spam_dir)
+
+    split_header(ham_emails[0])
+    ##    processed_ham = []
